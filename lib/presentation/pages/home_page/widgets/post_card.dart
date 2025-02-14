6fc94fe732b284bd715844437d484acb166b5d64
Alexander Aks'onov <m.mirvua@gmail.com>
2025-02-14 10:31:03 +0200

    made card shadow softer;

--- lib/presentation/pages/home_page/widgets/post_card.dart
+++ lib/presentation/pages/home_page/widgets/post_card.dart
@@ -25,9 +25,9 @@ class PostCard extends StatelessWidget {
         border: Border.all(width: 4.0, color: Colors.white,),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.2),
-            blurRadius: 20,
-            spreadRadius: 2,
+            color: Colors.black.withOpacity(0.17),
+            blurRadius: 30,
+            spreadRadius: 1,
             offset: Offset(0, 5),
           ),
         ],
