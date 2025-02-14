bd048c3f66bac545941e4ce7765a0b9371c71e75
Alexander Aks'onov <m.mirvua@gmail.com>
2025-02-14 12:41:53 +0200

    minor changes;

--- lib/presentation/pages/article_page/article_page.dart
+++ lib/presentation/pages/article_page/article_page.dart
@@ -1,5 +1,7 @@
+import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 
+@RoutePage()
 class ArticlePage extends StatefulWidget {
   const ArticlePage({super.key});
 
