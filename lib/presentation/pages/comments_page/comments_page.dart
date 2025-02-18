ea52497608db5038a5f9b8b76a543d67c9e0e1b5
Alexander Aks'onov <m.mirvua@gmail.com>
2025-02-18 12:23:58 +0200

    working on the comments page;

--- lib/presentation/pages/comments_page/comments_page.dart
+++ lib/presentation/pages/comments_page/comments_page.dart
@@ -1,4 +1,6 @@
 import 'package:auto_route/annotations.dart';
+import 'package:blogify_flutter_main/presentation/pages/home_page/widgets/rounded_button.dart';
+import 'package:blogify_flutter_main/presentation/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -16,13 +18,35 @@ class CommentsPage extends StatefulWidget {
 }
 
 class _CommentsPageState extends State<CommentsPage> {
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+
+    return Scaffold(
       body: Column(
         children: [
 
           //comment field
+          Container(
+            child: Row(
+              children: [
+
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'What are your thoughts?',
+                    hintStyle: TextStyle(
+                      color: AppColors.accentColor,
+                      fontSize: 14.0,
+                    ),
+                  ),
+
+                ),
+
+                const RoundedButton(text: 'Respond',),
+
+              ],
+            ),
+          ),
 
           //comments or a placeholder
 
