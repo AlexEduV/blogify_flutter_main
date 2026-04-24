3d544ff6b92f0d1ad8dd2c84f726a9a9b43f22c6
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-24 17:29:37 +0300

    switched to the external swipe package;

--- lib/presentation/pages/home_page/home_page.dart
+++ lib/presentation/pages/home_page/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:blogify_flutter_main/data/mock_storage/global_mock_storage_provider.dart';
 import 'package:blogify_flutter_main/presentation/pages/home_page/notifiers/category_index_notifier.dart';
@@ -8,6 +10,7 @@ import 'package:blogify_flutter_main/presentation/pages/home_page/widgets/post_c
 import 'package:blogify_flutter_main/presentation/pages/home_page/widgets/user_welcome_row.dart';
 import 'package:blogify_flutter_main/router/router.gr.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import 'package:provider/provider.dart';
 
 @RoutePage()
@@ -28,9 +31,17 @@ class _HomePageState extends State<HomePage> {
   //todo: on the emulator (Pixel 4), the paddings of the card stack are not right,
   // the bottom cards are not visible
 
+  final CardSwiperController controller = CardSwiperController();
+
   @override
   void initState() {
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -59,53 +70,25 @@ class _HomePageState extends State<HomePage> {
                     return const EmptyPostsPlaceholder();
                   }
 
-                  double getScale(int index, int length, double scaleStep) =>
-                      1.0 - ((length - 1 - index) * scaleStep);
-
-                  double getTopOffset(int index, int length, double baseOffset) =>
-                      (length - 1 - index) * baseOffset;
-
                   final visibleCount = 3;
-                  final visibleList = posts.take(visibleCount).toList();
 
                   //post stack
                   return Consumer<CategoryIndexNotifier>(
                       builder: (_, categoryIndexNotifier, child) {
-                    return Stack(
-                      clipBehavior: Clip.none,
-                      alignment: AlignmentGeometry.topCenter,
-                      children: List.generate(visibleList.length, (index) {
-                        final post = visibleList[index];
-
-                        return AnimatedPositioned(
-                          key: ValueKey(post.id),
-                          duration: const Duration(milliseconds: 400),
-                          curve: Curves.easeInOut,
-                          top: getTopOffset(index, visibleCount, 65),
-                          left: 0,
-                          right: 0,
-                          child: AnimatedScale(
-                            duration: const Duration(milliseconds: 400),
-                            curve: Curves.easeInOut,
-                            scale: getScale(index, visibleCount, 0.2),
-                            child: Dismissible(
-                              key: ValueKey(post.id),
-                              direction: DismissDirection.vertical,
-                              onDismissed: (direction) {
-                                notifier.removePostById(post.id);
-
-                                Future.delayed(const Duration(milliseconds: 400), () {
-                                  notifier.addPostBack(post);
-                                });
-                              },
-                              child: PostCard(
-                                post: post,
-                                onTap: openArticlePage,
-                              ),
-                            ),
-                          ),
-                        );
-                      }),
+                    return CardSwiper(
+                      controller: controller,
+                      cardsCount: posts.length,
+                      onSwipe: (_, __, direction) => true,
+                      numberOfCardsDisplayed: limitedCount(visibleCount),
+                      backCardOffset: const Offset(0, 40),
+                      padding: EdgeInsets.zero,
+                      cardBuilder: (
+                        context,
+                        index,
+                        horizontalThresholdPercentage,
+                        verticalThresholdPercentage,
+                      ) =>
+                          PostCard(post: posts[index], onTap: openArticlePage),
                     );
                   });
                 }),
@@ -124,4 +107,6 @@ class _HomePageState extends State<HomePage> {
       id: id,
     ));
   }
+
+  int limitedCount(int length, [int max = 3]) => min(length, max);
 }
