99dfdf3e6508883c4279a1419e3efb1e38080eaa
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-27 17:10:50 +0300

    test changes;

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,6 @@ import 'data/providers/global_mock_comment_provider.dart';
 import 'data/providers/global_mock_storage_provider.dart';
 
 void main() {
-  //test changes;
-
   initDependenciesContainer();
 
   runApp(MultiProvider(
