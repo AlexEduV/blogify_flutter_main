7f31333b7fe7e4f1114733c41256148571cb1656
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-28 12:41:10 +0300

    moved tab indices to consts;

--- lib/common/app_constants.dart
+++ lib/common/app_constants.dart
@@ -1,3 +1,7 @@
 class AppConstants {
   static const homePagePostsVisibleCount = 3;
+
+  static const homeTabTrendingIndex = 0;
+  static const homeTabDesignIndex = 1;
+  static const homeTabTechIndex = 2;
 }
--- lib/common/enums/post_category.dart
+++ lib/common/enums/post_category.dart
@@ -1,9 +1,11 @@
 import 'package:blogify_flutter_main/l10n/l10n.dart';
 
+import '../app_constants.dart';
+
 enum PostCategory {
-  design(tabIndex: 1, label: L10n.postsCategoryDesign),
-  tech(tabIndex: 2, label: L10n.postsCategoryTech),
-  trending(tabIndex: 0, label: L10n.postsCategoryTrending),
+  design(tabIndex: AppConstants.homeTabDesignIndex, label: L10n.postsCategoryDesign),
+  tech(tabIndex: AppConstants.homeTabTechIndex, label: L10n.postsCategoryTech),
+  trending(tabIndex: AppConstants.homeTabTrendingIndex, label: L10n.postsCategoryTrending),
   none();
 
   final int? tabIndex;
