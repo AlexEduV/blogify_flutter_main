d1c83ce1e7b4743fe7c7522db117cd392fc3bb39
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-28 17:43:43 +0300

    updated todo;

--- lib/presentation/pages/article_page/article_page.dart
+++ lib/presentation/pages/article_page/article_page.dart
@@ -134,7 +134,7 @@ class _ArticlePageState extends State<ArticlePage> {
   }
 
   Future<void> onShareButtonPressed(PostEntity post) async {
-    //todo: the content could be better and also better to call notifier -> use case here;
+    //todo: better to call notifier -> use case here;
 
     final model = ShareParamsModel(
         title: '${L10n.appName} | ${post.title}',
