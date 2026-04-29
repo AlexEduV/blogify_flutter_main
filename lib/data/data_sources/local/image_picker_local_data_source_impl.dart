9e5014617b3823fc8174b019333f7020bb63a95b
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-29 15:30:16 +0300

    added image picker data source;

---
+++ lib/data/data_sources/local/image_picker_local_data_source_impl.dart
@@ -0,0 +1,14 @@
+import 'package:blogify_flutter_main/domain/data_sources/local/image_picker_local_data_source.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImagePickerLocalDataSourceImpl implements ImagePickerLocalDataSource {
+  final ImagePicker _imagePicker;
+
+  ImagePickerLocalDataSourceImpl(this._imagePicker);
+
+  @override
+  Future<String?> pickImage() async {
+    final file = await _imagePicker.pickImage(source: ImageSource.gallery);
+    return file?.path;
+  }
+}
