01a28674732a12a49c4172aae2bad32b637d72e3
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-30 10:21:13 +0300

    refactoring in the user photo;

--- lib/presentation/widgets/user_photo.dart
+++ lib/presentation/widgets/user_photo.dart
@@ -20,47 +20,46 @@ class UserPhoto extends StatelessWidget {
   Widget build(BuildContext context) {
     final image = getUserImage();
 
-    return Center(
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          Container(
-            width: size,
-            height: size,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: Colors.white, // Set your desired border color
-                width: 3.0, // Set your desired border width
-              ),
-              image: image != null
-                  ? DecorationImage(image: image, fit: BoxFit.cover, alignment: Alignment.topCenter)
-                  : null,
+    return Stack(
+      alignment: AlignmentGeometry.center,
+      children: [
+        Container(
+          width: size,
+          height: size,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: Colors.white, // Set your desired border color
+              width: 3.0, // Set your desired border width
             ),
+            image: image != null
+                ? DecorationImage(image: image, fit: BoxFit.cover, alignment: Alignment.topCenter)
+                : null,
           ),
-          if (onTap != null) ...[
-            Positioned(
-              bottom: 0,
-              right: 0,
-              child: Material(
-                color: AppColors.accentColor,
-                shape: const CircleBorder(),
-                child: InkWell(
-                  customBorder: const CircleBorder(),
-                  splashColor: AppColors.accentColor.withAlpha(60),
-                  highlightColor: Colors.transparent,
-                  onTap: onTap,
-                  child: const CircleAvatar(
-                    backgroundColor: Colors.grey,
-                    radius: 20,
-                    child: Icon(Icons.edit, color: Colors.white, size: 20 * 1.2),
-                  ),
+        ),
+        if (onTap != null) ...[
+          //todo: the button does not show the splash effect;
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Material(
+              color: AppColors.accentColor,
+              shape: const CircleBorder(),
+              child: InkWell(
+                customBorder: const CircleBorder(),
+                splashColor: AppColors.accentColor.withAlpha(60),
+                highlightColor: Colors.transparent,
+                onTap: onTap,
+                child: const CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 20,
+                  child: Icon(Icons.edit, color: Colors.white, size: 20 * 1.2),
                 ),
               ),
             ),
-          ],
+          ),
         ],
-      ),
+      ],
     );
   }
 
