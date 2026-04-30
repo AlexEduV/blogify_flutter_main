d434ea73beac22ea6ddd51f616068d95ca696f54
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-30 10:08:53 +0300

    updated initial user data;

--- lib/data/data_sources/remote/mock_users_data_source_impl.dart
+++ lib/data/data_sources/remote/mock_users_data_source_impl.dart
@@ -10,18 +10,34 @@ class MockUsersDataSourceImpl implements UsersDataSource {
   void init() {
     users = [
       const UserEntity(
-          id: 2, firstName: 'Alex', lastName: 'McKey', email: 'alexmckey@gmail.com', imageSrc: ''),
+        id: 2,
+        firstName: 'Alex',
+        lastName: 'McKey',
+        email: 'alexmckey@gmail.com',
+        imageSrc: '',
+      ),
       const UserEntity(
-          id: 3, firstName: 'Tom', lastName: 'Noble', email: 'tomnb23@gmail.com', imageSrc: ''),
+        id: 3,
+        firstName: 'Tom',
+        lastName: 'Noble',
+        email: 'tomnb23@gmail.com',
+        imageSrc: '',
+      ),
       const UserEntity(
-          id: 4, firstName: 'Marney', lastName: 'March', email: 'mmarch@gmail.com', imageSrc: ''),
+        id: 4,
+        firstName: 'Marney',
+        lastName: 'March',
+        email: 'mmarch@gmail.com',
+        imageSrc: '',
+      ),
       const UserEntity(
         id: 1,
         firstName: 'Jude',
-        lastName: 'Adams',
-        email: 'therealjude@gmail.com',
-        imageSrc: '',
-      )
+        lastName: 'Jacob',
+        email: 'therealjude@disney.com',
+        imageSrc:
+            'https://www.shutterstock.com/editorial/image-editorial/OdTac830M9T7Uay6OTQ5Nw==/hayden-byerly-440nw-9699540aa.jpg',
+      ),
     ];
   }
 
